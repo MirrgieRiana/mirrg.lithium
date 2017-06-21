ed7d0ce295bd5785147f617a7b701dd7581480dc
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-06-22 06:12:20 +0900

    add: Launcher

---
+++ mirrg.applications.service.pwi/src/main/java/mirrg/applications/service/pwi/Launcher.java
@@ -0,0 +1,18 @@
+package mirrg.applications.service.pwi;
+
+import mirrg.lithium.properties.Properties;
+
+public class Launcher
+{
+
+	public Launcher(Properties properties)
+	{
+
+	}
+
+	public void start()
+	{
+
+	}
+
+}
