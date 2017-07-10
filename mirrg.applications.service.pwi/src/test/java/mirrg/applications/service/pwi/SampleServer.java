36631515c683aafef11b50f81c77a0e46b91ed2f
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-07-11 01:11:42 +0900

    pwi: テスト用propertiesを分離

---
+++ mirrg.applications.service.pwi/src/test/java/mirrg/applications/service/pwi/SampleServer.java
@@ -0,0 +1,13 @@
+package mirrg.applications.service.pwi;
+
+public class SampleServer
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		Main.main(new String[] {
+			"test.pwi.properties",
+		});
+	}
+
+}
