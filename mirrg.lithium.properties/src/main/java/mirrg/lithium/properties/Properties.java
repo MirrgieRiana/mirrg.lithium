14295d8a1ff232e670c2f9a935725067f78bc11d
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-07-14 00:29:30 +0900

    properties: 3.1.1

--- mirrg.lithium.properties/src/main/java/mirrg/lithium/properties/Properties.java
+++ mirrg.lithium.properties/src/main/java/mirrg/lithium/properties/Properties.java
@@ -3,9 +3,8 @@ package mirrg.lithium.properties;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Optional;
-import java.util.function.Function;
 
-public class Properties
+public class Properties implements IProperties
 {
 
 	private ArrayList<Properties> parents = new ArrayList<>();
@@ -26,22 +25,6 @@ public class Properties
 		methods.put(key, p -> value);
 	}
 
-	public String get(String key)
-	{
-		return getString(key).get();
-	}
-
-	public Optional<IMethod> getMethod(String key)
-	{
-		IMethod method = methods.get(key);
-		if (method != null) return Optional.of(method);
-		for (Properties parent : parents) {
-			Optional<IMethod> oMethod = parent.getMethod(key);
-			if (oMethod.isPresent()) return oMethod;
-		}
-		return Optional.empty();
-	}
-
 	public Optional<IMethod> getMethodOfParents(String key)
 	{
 		for (Properties parent : parents) {
@@ -51,6 +34,14 @@ public class Properties
 		return Optional.empty();
 	}
 
+	public Optional<IMethod> getMethod(String key)
+	{
+		IMethod method = methods.get(key);
+		if (method != null) return Optional.of(method);
+		return getMethodOfParents(key);
+	}
+
+	@Override
 	public Optional<String> getString(String key)
 	{
 		return getMethod(key).map(m -> m.apply(this));
@@ -59,55 +50,6 @@ public class Properties
 	public Optional<String> getStringOfParents(String key)
 	{
 		return getMethodOfParents(key).map(m -> m.apply(this));
-	}
-
-	private <T> Optional<T> getNumber(String key, Function<String, T> function)
-	{
-		try {
-			return getString(key).map(s -> function.apply(s));
-		} catch (NumberFormatException e) {
-			return Optional.empty();
-		}
-	}
-
-	public Optional<Byte> getByte(String key)
-	{
-		return getNumber(key, s -> Byte.parseByte(s, 10));
-	}
-
-	public Optional<Short> getShort(String key)
-	{
-		return getNumber(key, s -> Short.parseShort(s, 10));
-	}
-
-	public Optional<Integer> getInteger(String key)
-	{
-		return getNumber(key, s -> Integer.parseInt(s, 10));
-	}
-
-	public Optional<Long> getLong(String key)
-	{
-		return getNumber(key, s -> Long.parseLong(s, 10));
-	}
-
-	public Optional<Float> getFloat(String key)
-	{
-		return getNumber(key, s -> Float.parseFloat(s));
-	}
-
-	public Optional<Double> getDouble(String key)
-	{
-		return getNumber(key, s -> Double.parseDouble(s));
-	}
-
-	public Optional<Boolean> getBoolean(String key)
-	{
-		return getString(key).map(Boolean::parseBoolean);
-	}
-
-	public Optional<Character> getChar(String key)
-	{
-		return getString(key).filter(s -> !s.isEmpty()).map(s -> s.charAt(0));
 	}
 
 }
