8d19c9f3316efa1bc723df75bfd29da3d61c36b6
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-09-10 05:56:35 +0900

    fix

--- mirrg.lithium.parser/src/main/java/mirrg/lithium/parser/syntaxes/Operation.java
+++ mirrg.lithium.parser/src/main/java/mirrg/lithium/parser/syntaxes/Operation.java
@@ -5,8 +5,8 @@ import mirrg.lithium.struct.ImmutableArray;
 public class Operation<OPERAND, OPERATOR>
 {
 
-	private ImmutableArray<OPERAND> operands;
-	private ImmutableArray<OPERATOR> operators;
+	public final ImmutableArray<OPERAND> operands;
+	public final ImmutableArray<OPERATOR> operators;
 
 	public Operation(ImmutableArray<OPERAND> operands, ImmutableArray<OPERATOR> operators)
 	{
@@ -14,40 +14,20 @@ public class Operation<OPERAND, OPERATOR>
 		this.operators = operators;
 	}
 
-	public OPERAND getOperand(int index)
-	{
-		return operands.get(index);
-	}
-
-	public OPERATOR getOperator(int index)
-	{
-		return operators.get(index);
-	}
-
-	public int getOperandCount()
-	{
-		return operands.length();
-	}
-
-	public int getOperatorCount()
-	{
-		return operators.length();
-	}
-
 	public OPERAND calculateLeft(ICalculator<OPERAND, OPERATOR> calculator)
 	{
-		OPERAND left = getOperand(0);
-		for (int i = 0; i < getOperatorCount(); i++) {
-			left = calculator.calculate(left, getOperator(i), getOperand(1 + i));
+		OPERAND left = operands.get(0);
+		for (int i = 0; i < operators.length(); i++) {
+			left = calculator.calculate(left, operators.get(i), operands.get(1 + i));
 		}
 		return left;
 	}
 
 	public OPERAND calculateRight(ICalculator<OPERAND, OPERATOR> calculator)
 	{
-		OPERAND right = getOperand(getOperatorCount());
-		for (int i = getOperatorCount() - 1; i >= 0; i--) {
-			right = calculator.calculate(getOperand(i), getOperator(i), right);
+		OPERAND right = operands.get(operators.length());
+		for (int i = operators.length() - 1; i >= 0; i--) {
+			right = calculator.calculate(operands.get(i), operators.get(i), right);
 		}
 		return right;
 	}
