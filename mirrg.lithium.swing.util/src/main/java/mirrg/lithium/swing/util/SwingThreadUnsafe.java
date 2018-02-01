804856c1b386e85ef2535ea7c4d3e994a3cd5d19
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-02-02 07:44:59 +0900

    swing.util: add: SwingThreadUnsafe

---
+++ mirrg.lithium.swing.util/src/main/java/mirrg/lithium/swing/util/SwingThreadUnsafe.java
@@ -0,0 +1,16 @@
+package mirrg.lithium.swing.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * このメソッドはAWTイベントディスパッチャースレッドから呼び出す必要があります。
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface SwingThreadUnsafe
+{
+
+}
