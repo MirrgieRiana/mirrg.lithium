ac4de01db00762f117e6dce151d4e397c948c9ba
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-02-01 09:10:03 +0900

    logging: add: OutputStreamLogging

--- mirrg.lithium.logging/src/test/java/mirrg/lithium/logging/TestLogger.java
+++ mirrg.lithium.logging/src/test/java/mirrg/lithium/logging/TestLogger.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.*;
 
 import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Optional;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -72,6 +76,73 @@ public class TestLogger
 			+ ".{23} \\[TRACE] 006" + System.lineSeparator()));
 		Thread.sleep(1000);
 		frame.dispose();
+	}
+
+	@Test
+	public void test_OutputStreamLogging() throws Exception
+	{
+		ArrayList<String> strings = new ArrayList<>();
+
+		test0(strings, "UTF-8");
+		test0(strings, "Shift-JIS");
+		test0(strings, "Unicode");
+
+		{
+			try (PrintStream out2 = new PrintStream(new OutputStreamLogging(new Logger() {
+				@Override
+				public void println(String string, Optional<EnumLogLevel> oLogLevel)
+				{
+					strings.add(string);
+				}
+			}, "Unicode"), true, "Unicode")) {
+
+				out2.println("abc");
+				assertEquals(1, strings.size());
+				assertEquals("abc", strings.get(0));
+				strings.clear();
+
+				out2.println("def");
+				assertEquals(1, strings.size());
+				assertEquals("def", strings.get(0));
+				strings.clear();
+
+				out2.println("ghi");
+				assertEquals(1, strings.size());
+				assertEquals("ghi", strings.get(0));
+				strings.clear();
+
+			}
+
+			assertEquals(1, strings.size());
+			assertEquals("", strings.get(0));
+			strings.clear();
+		}
+
+	}
+
+	private void test0(ArrayList<String> strings, String charset) throws UnsupportedEncodingException
+	{
+		try (PrintStream out2 = new PrintStream(new OutputStreamLogging(new Logger() {
+			@Override
+			public void println(String string, Optional<EnumLogLevel> oLogLevel)
+			{
+				strings.add(string);
+			}
+		}, charset), true, charset)) {
+
+			out2.print("あいうえお\nかきく\rけ\r\nこ");
+			out2.flush();
+			assertEquals(3, strings.size());
+			assertEquals("あいうえお", strings.get(0));
+			assertEquals("かきく", strings.get(1));
+			assertEquals("け", strings.get(2));
+			strings.clear();
+
+		}
+
+		assertEquals(1, strings.size());
+		assertEquals("こ", strings.get(0));
+		strings.clear();
 	}
 
 }
